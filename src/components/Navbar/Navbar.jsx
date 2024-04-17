4c014adf2cf4faf7381a0950b3d8d261efb3fe7d
Toyosi Odukale <88277081+Rei-ne@users.noreply.github.com>
2024-04-17 21:28:38 +0100

    feat(005): working on mobile nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./Navbar.css";
+
 import Searchbar from "../Searchbar/Searchbar";
 
 export default function Navbar() {
+  // const [showNav, setShowNav] = useState(true)
+
   return (
     <div className="navbar">
-      <nav>
+      {/* desktop nav */}
+      <nav className="desktop">
+        <Link to="/" className="brand">
+          <h1>Show Boss</h1>
+        </Link>
+        <Searchbar />
+        <Link to="/create">
+          <h1>Create Show</h1>
+        </Link>
+      </nav>
+
+      {/* mobile nav */}
+      <nav className="mobile">
         <Link to="/" className="brand">
           <h1>Show Boss</h1>
         </Link>
